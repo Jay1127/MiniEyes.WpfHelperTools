9ea0b501506875fc849519c0c105c2a72663be9c
oksujay1127 <oksujay1127@gmail.com>
2020-03-20 11:18:11 +0900

    [Feature] CheckBox Accent 색상 변경 기능 추가

---
+++ src/MiniEyes.WpfHelperTools/Controls/Buttons/FluentCheckBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MiniEyes.WpfHelperTools.Controls.Buttons
+{
+    internal class FluentCheckBox : CheckBox
+    {
+        public DataTemplate AccentTemplate
+        {
+            get { return (DataTemplate)GetValue(AccentTemplateProperty); }
+            set { SetValue(AccentTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AccentTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AccentTemplateProperty =
+            DependencyProperty.Register("AccentTemplate", typeof(DataTemplate), typeof(MiniCheckBox));
+
+    }
+}
---
+++ src/MiniEyes.WpfHelperTools/Controls/Buttons/MiniCheckBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MiniEyes.WpfHelperTools
+{
+    public class MiniCheckBox : CheckBox
+    {
+        public Brush AccentColor
+        {
+            get { return (Brush)GetValue(AccentColorProperty); }
+            set { SetValue(AccentColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty AccentColorProperty =
+            DependencyProperty.Register("AccentColor", typeof(Brush), typeof(MiniCheckBox), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+    }
+}
